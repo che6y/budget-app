6cd15d8e97ac7c84bc8e2e0595eafea9cb272668
che6y <che6yforever@gmail.com>
2019-01-05 20:22:17 +0700

    New project

---
+++ project1/database/migrations/2019_01_02_110220_create_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('purchase_id')->unsigned();
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+}
